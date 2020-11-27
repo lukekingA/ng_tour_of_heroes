6d6135485a4029a39fb89e673512b3fd7d38c924
Luke King <lxk@kount.com>
2020-11-27 01:48:35 -0700

    added heroes and mock heroes

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../models/hero'
+import { Heroes } from '../../data/mock-heros'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  tempName = ''
+
+  heroes = Heroes
+  // hero: Hero = {
+  //   id:1,
+  //   name: 'Windstorm'
+  // }
+
+  updateHeroName(id) {
+    for(let i = 1; i <= this.heroes.length; i++){
+      if (id == this.heroes[i].id){
+        console.log("IM HERE")
+        this.heroes[i].name = this.tempName
+        this.tempName = ''
+      }
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
